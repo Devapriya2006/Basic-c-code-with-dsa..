05a483bf4b577d05b0b0101ea35d3d4f1bcf452b
DEVAPRIYA PAUL KUNDU <devapriya2006paul@gmail.com>
2026-03-10 22:29:27 +0530

    Add age check program to cpp3.cpp

---
+++ cpp3.cpp
@@ -0,0 +1,16 @@
+// adult.
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+cout<< "enter your age: ";
+cin>> a;
+if (a>=18)
+{
+    cout<<"adult";
+}
+else{
+    cout<< "minor";
+}
+return 0;
+}
