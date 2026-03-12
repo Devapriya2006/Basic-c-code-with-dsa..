bd1ecbc42be2ebe81fff3f11b90c0e44261dd357
DEVAPRIYA PAUL KUNDU <devapriya2006paul@gmail.com>
2026-03-12 14:12:16 +0530

    Add cpp16.cpp to sum two numbers

---
+++ cpp16.cpp
@@ -0,0 +1,14 @@
+// function add two numbers
+#include<iostream>
+using namespace std;
+void add(int a,int b){
+    cout<< "the sum is :"<< a+b << endl;
+}
+int main(){
+    add(5,3);
+    add(2,3);
+    add(4,2);
+    add(6,3);
+    add(7,8);
+    return 0;
+}
