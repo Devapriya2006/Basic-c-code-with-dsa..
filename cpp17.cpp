3d14d1861c94bc145063ba36031871425d4c8ce5
DEVAPRIYA PAUL KUNDU <devapriya2006paul@gmail.com>
2026-03-12 14:14:30 +0530

    Add message function to print greeting

---
+++ cpp17.cpp
@@ -0,0 +1,9 @@
+// function and string passing
+#include<iostream>
+using namespace std;
+ void message(string msg){
+    cout << "hello everyone--" << msg << endl;
+ }
+ int main(){
+    message("how are u??");
+ }
