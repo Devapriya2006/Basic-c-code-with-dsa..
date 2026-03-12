19cceabc2854f64187c8cd73863f3c8733c23994
DEVAPRIYA PAUL KUNDU <devapriya2006paul@gmail.com>
2026-03-12 14:11:38 +0530

    Add Devapriya function to print greeting

---
+++ cpp15.cpp
@@ -0,0 +1,11 @@
+//function
+#include<iostream>
+using namespace std;
+// declare function
+void Devapriya(){// function name Devapriya
+    cout<<"Hello tpi ):" << endl; // output
+}
+int main(){
+    Devapriya();// calling Devapriya 
+    return 0;
+}
