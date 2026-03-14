b6da81428fdb3d6093a081e020efbffd98159c5a
DEVAPRIYA PAUL KUNDU <devapriya2006paul@gmail.com>
2026-03-15 00:02:31 +0530

    Add simple string display program in cpp30.cpp

---
+++ cpp30.cpp
@@ -0,0 +1,10 @@
+// simple string display using array
+#include<iostream>
+using namespace std;
+int main(){
+    char name[15];
+    cout<< "Enter your name:";
+    cin>> name;
+    cout<< "your name is:"<< name;
+    return 0;
+}
